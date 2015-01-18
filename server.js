5216b97caf65cf5082e2060632c1fec328615140
anj <anuj@ubuntu.ubuntu-domain>
2015-01-18 23:27:03 +0530

    Node Server Created

---
+++ server.js
@@ -0,0 +1,16 @@
+/*Author:Anuj Sachan
+URL: www.github.com/anujsachan1990
+Website:www.anujsachan.com
+this for creating server with Node Js
+*/
+
+var http = require("http");
+// module require for creating server
+var server = http.createServer(function(request,response){
+		response.writeHead(200 ,{"content-type":"text/html"});
+		response.write("<h1>IT Works...</h1>");
+		response.write("<h3>Welcome to very first Node Server</h3>");
+		response.end();
+});
+server.listen(5000);
+console.log("Server is now listen on http://localhost:5000");
